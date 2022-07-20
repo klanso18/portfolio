de5ecd39ef740aaaaa4a298b9b133fa77462e675
Kevin Lansot <KEVIN.LANSOT@gmail.com>
2022-07-20 23:22:28 +0200

    project page updated

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -2,10 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Project;
+use App\Repository\ImageRepository;
 use App\Repository\ProjectRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\StackRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ProjectController extends AbstractController
 {
@@ -15,6 +18,18 @@ class ProjectController extends AbstractController
         $projects = $projectRepository->findAll();
         return $this->render('project/index.html.twig', [
             'projects' => $projects
+        ]);
+    }
+
+    #[Route('project/{id}', name: 'project_show')]
+    public function show(Project $project, ImageRepository $imageRepository, StackRepository $stackRepository): Response
+    {
+        $images = $imageRepository->findAll();
+        $stacks = $stackRepository->findAll();
+        return $this->render('project/show.html.twig', [
+            'project' => $project,
+            'images' => $images,
+            'stacks' => $stacks
         ]);
     }
 }
--- src/DataFixtures/ImageFixtures.php
+++ src/DataFixtures/ImageFixtures.php
@@ -10,35 +10,22 @@ use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 class ImageFixtures extends Fixture implements DependentFixtureInterface
 {
     public const IMAGES = [
-        ['name' => 'cv_home.png', 'is_main' => true, 'project' => 'project_0'],
+        ['name' => 'cv_home2.png', 'is_main' => true, 'project' => 'project_0'],
         ['name' => 'cv_back.png', 'is_main' => false, 'project' => 'project_0'],
         ['name' => 'cv_skills.png', 'is_main' => false, 'project' => 'project_0'],
         ['name' => 'cv_contact.png', 'is_main' => false, 'project' => 'project_0'],
-        ['name' => 'cv_resp-home.png', 'is_main' => false, 'project' => 'project_0'],
-        ['name' => 'cv_resp-skills.png', 'is_main' => false, 'project' => 'project_0'],
-        ['name' => 'cv_resp-pic.png', 'is_main' => false, 'project' => 'project_0'],
-        ['name' => 'cv_resp-contact.png', 'is_main' => false, 'project' => 'project_0'],
-        ['name' => 'ss_home2.png', 'is_main' => true, 'project' => 'project_1'],
+        ['name' => 'ss_home.png', 'is_main' => true, 'project' => 'project_1'],
         ['name' => 'ss_connex.png', 'is_main' => false, 'project' => 'project_1'],
         ['name' => 'ss_register.png', 'is_main' => false, 'project' => 'project_1'],
         ['name' => 'ss_index.png', 'is_main' => false, 'project' => 'project_1'],
         ['name' => 'ss_show.png', 'is_main' => false, 'project' => 'project_1'],
         ['name' => 'ss_show-own.png', 'is_main' => false, 'project' => 'project_1'],
         ['name' => 'ss_edit.png', 'is_main' => false, 'project' => 'project_1'],
-        ['name' => 'ss_resp-home.png', 'is_main' => false, 'project' => 'project_1'],
-        ['name' => 'ss_resp-index.png', 'is_main' => false, 'project' => 'project_1'],
-        ['name' => 'ss_resp-show.png', 'is_main' => false, 'project' => 'project_1'],
-        ['name' => 'ss_resp-add.png', 'is_main' => false, 'project' => 'project_1'],
-        ['name' => 'pote_home2.png', 'is_main' => true, 'project' => 'project_2'],
+        ['name' => 'pote_home.png', 'is_main' => true, 'project' => 'project_2'],
         ['name' => 'pote_cat2.png', 'is_main' => false, 'project' => 'project_2'],
         ['name' => 'pote_index.png', 'is_main' => false, 'project' => 'project_2'],
         ['name' => 'pote_add.png', 'is_main' => false, 'project' => 'project_2'],
         ['name' => 'pote_register.png', 'is_main' => false, 'project' => 'project_2'],
-        ['name' => 'pote_resp-home.png', 'is_main' => false, 'project' => 'project_2'],
-        ['name' => 'pote_resp-connex.png', 'is_main' => false, 'project' => 'project_2'],
-        ['name' => 'pote_resp-cat.png', 'is_main' => false, 'project' => 'project_2'],
-        ['name' => 'pote_resp-index.png', 'is_main' => false, 'project' => 'project_2'],
-        ['name' => 'pote_resp-new.png', 'is_main' => false, 'project' => 'project_2'],
         ['name' => 'ev_home.png', 'is_main' => true, 'project' => 'project_3'],
         ['name' => 'ev_connexion.png', 'is_main' => false, 'project' => 'project_3'],
         ['name' => 'ev_welcome.png', 'is_main' => false, 'project' => 'project_3'],
@@ -48,10 +35,7 @@ class ImageFixtures extends Fixture implements DependentFixtureInterface
         ['name' => 'ev_graph.png', 'is_main' => false, 'project' => 'project_3'],
         ['name' => 'ev_admin.png', 'is_main' => false, 'project' => 'project_3'],
         ['name' => 'ev_list.png', 'is_main' => false, 'project' => 'project_3'],
-        ['name' => 'ev_voter.png', 'is_main' => false, 'project' => 'project_3'],
-        ['name' => 'ev_welcomevote.png', 'is_main' => false, 'project' => 'project_3'],
-        ['name' => 'ev_vote1.png', 'is_main' => false, 'project' => 'project_3'],
-        ['name' => 'ev_vote2.png', 'is_main' => false, 'project' => 'project_3']
+        ['name' => 'ev_voter.png', 'is_main' => false, 'project' => 'project_3']
     ];
 
     public function load(ObjectManager $manager): void
