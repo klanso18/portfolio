423e5765141b05b6a892a7a1e8f0cc98072a9ddc
Kevin Lansot <KEVIN.LANSOT@gmail.com>
2022-07-21 13:23:59 +0200

    user and admin feature updated

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher) 
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $admin = new User();
+        $admin->setEmail('admin@mail.com');
+        $admin->setPassword($this->passwordHasher->hashPassword($admin, 'password'));
+        $admin->setRoles(['ROLE_ADMIN']);
+        $manager->persist($admin);
+        $manager->flush();
+    }
+}
---
+++ src/Form/ProjectType.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Stack;
+use App\Entity\Project;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class ProjectType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Nom du projet',
+                'help' => 'Merci de rentrer le nom du projet'
+            ])
+            ->add('description', TextType::class, [
+                'label' => 'Description (optionnel)',
+                'required' => false,
+                'help' => 'Merci de rentrer une description'
+            ])
+            ->add('createdAt', DateType::class, [
+                'label' => 'Created date',
+                'widget' => 'single_text'
+            ])
+            // ->add('stack', EntityType::class, [
+            //     'class' => Stack::class,
+            //     'choice_label' => 'name',
+            //     'multiple' => true,
+            //     'expanded' => true,
+            //     'by_reference' => false,
+            // ]);
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Project::class,
+        ]);
+    }
+}
